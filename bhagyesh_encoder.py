26235e2305cdcfd521a1c74ce67c07db3373d5b5
Bhagyesh Jethwani <127056352+Babagesh@users.noreply.github.com>
2023-03-07 21:52:02 -0500

    Add files via upload

---
+++ bhagyesh_encoder.py
@@ -0,0 +1,21 @@
+#
+
+def encoder():
+    pass
+
+
+def decoder():
+    pass
+
+
+if __name__ == '__main__':
+    exit_program = False
+    while not exit_program:
+        print("Menu")
+        print("---------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        user_option = input("Please enter an option: ")
+        password = input("Please enter your password to encode: ")
+        
